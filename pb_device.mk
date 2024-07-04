b96a344f4671c5efde55fa91b72ad3b41830850e
alterego <125763784+senyaml@users.noreply.github.com>
2024-07-05 05:55:24 +0800

    Update pb_device.mk

--- pb_device.mk
+++ pb_device.mk
@@ -7,17 +7,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) -- only for 64bit phones
 
 # Inherit from device
-$(call inherit-product, device/<path>/device.mk) -- path to main device makefile
+$(call inherit-product, device/xiaomi/twrp_emerald.mk) -- path to main device makefile
 
 # Inherit common product files.
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
-BOARD_VENDOR := 
-PRODUCT_BRAND := 
-PRODUCT_DEVICE := 
-PRODUCT_NAME := pb_device
-PRODUCT_MANUFACTURER := 
-PRODUCT_MODEL := 
-TARGET_VENDOR := 
+BOARD_VENDOR := mt6789
+PRODUCT_BRAND := POCO
+PRODUCT_DEVICE := emerald
+PRODUCT_NAME := pb_emerald
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := emerald
+TARGET_VENDOR := emerald
 ```
