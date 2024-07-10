b0ea55613a7e1df99874c9b740a7036e35ccc6f3
alterego <125763784+senyaml@users.noreply.github.com>
2024-07-10 12:03:05 -0700

    v

--- device.mk
+++ device.mk
@@ -94,9 +94,14 @@ PRODUCT_PACKAGES += \
 
 # Additional configs
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/vendor.hardware.vibratorfeature.IVibratorExt-V1-ndk_platform.so \
+	
+	
+	
+TARGET_RECOVERY_DEVICE_MODULES += libexpat
+RECOVERY_LIBRARY_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libexpat.so
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
